11a06e3cecba31bb9359dfcf245f5fc311a8767a
sanidhya8897 <164610648+sanidhya8897@users.noreply.github.com>
2024-10-30 09:44:26 +0530

    Add files via upload

---
+++ Pattern1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int n,i,j;
+ printf("enter the values");
+ scanf("%d",&n);
+ for(i = 1; i<= n; i++)
+ {
+    printf("1 2 3 4 5",i);
+    
+    for(j = 0; j<= 0; j++)
+    {
+        printf("\n",j);
+    }
+ }    
+
+ return 0;
+}
